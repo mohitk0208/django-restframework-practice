18ea5d3ee758dcf59977dd158ed81300530104e0
Mohit Kumar <mohitk0208@users.noreply.github.com>
2021-05-06 12:44:36 +0000

    completed tutorial 4:Authentication
    linking snippet with the users and adding permissions

--- snippets/views.py
+++ snippets/views.py
@@ -1,26 +1,32 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer,UserSerializer
 from django.contrib.auth.models import User
-
+from rest_framework import permissions
 from rest_framework import generics
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
-    queryset = Snippet.objects.all()
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class UserDetail(generics.RetrieveAPIView):
-    queryset = Snippet.objects.all()
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
 

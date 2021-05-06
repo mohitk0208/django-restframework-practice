286e54b056568133320b05f986c05d5231274876
Mohit Kumar <mohitk0208@users.noreply.github.com>
2021-05-06 09:48:51 +0000

    initial commit

---
+++ snippets/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('snippets/',views.SnippetList.as_view()),
+    path('snippets/<int:pk>/',views.SnippetDetail.as_view()),
+    path('users/',views.UserList.as_view()),
+    path('users/<int:pk>',views.UserDetail.as_view())
+]
+
+
+#---------------------- with function views----------------
+
+# urlpatterns = [
+#     path('snippets/',views.snippet_list),
+#     path('snippets/<int:pk>',views.snippet_detail)
+# ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
